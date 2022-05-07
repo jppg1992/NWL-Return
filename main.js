6d32908ae8df26bbd75c29d27ca602e845a492bb
João Paulo Godinho <j_paulo_g@hotmail.com>
2022-05-07 00:07:22 -0300

    aula dia 5

--- main.js
+++ main.js
@@ -3,10 +3,51 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  //linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  //topo da section
+  const sectionTop = section.offsetTop
+  //altura da section
+  const sectionHeight = section.offsetHeight
+
+  //verificar se o topo passou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  console.log('o topo Passou da linha?', sectionTopReachOrPassedTargetLine)
+
+  //fim da section
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //verificar se o final da section passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  console.log('o fundo Passou da linha?', sectionEndPassedTargetLine)
+
+  //limites da section
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  //pega o id=apelido da section
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
-  if (scrollY > 1000) {
+  if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
